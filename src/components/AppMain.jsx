b2b1e4b51b45d72605ec53b80c21c3fe5e8c4f8e
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-02 21:20:51 +0100

    Feat: Form add new movie

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -11,7 +11,7 @@ function AppMain() {
 
     const [search, setSearch] = useState("");
     const [filteredMovieList, setFilteredMovieList] = useState(moviesListWithId);
-
+    const [newMovie, setNewMovie] = useState("");
 
     useEffect(() => {
         const newArray = moviesListWithId.filter((movie) =>
@@ -19,6 +19,16 @@ function AppMain() {
         );
         setFilteredMovieList(newArray);
     }, [search]);
+
+    function createNewMovie(event) {
+        event.preventDefault();
+        const newMovie = {
+            id: posts.length > 0 ? posts[posts.length - 1].id + 1 : 1,
+            title: newMovie,
+        };
+        setPosts((current) => [...current, nuovoPost]);
+        setNewPost("");
+    }
 
     return (
         <>
@@ -28,7 +38,7 @@ function AppMain() {
                     <div className="flex py-20">
                         <span class="mx-10">
                             <svg width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.415l-3.85-3.85zm-5.242 1.656a5 5 0 1 1 0-10 5 5 0 0 1 0 10z"/>
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.415l-3.85-3.85zm-5.242 1.656a5 5 0 1 1 0-10 5 5 0 0 1 0 10z" />
                             </svg>
                         </span>
 
@@ -39,8 +49,9 @@ function AppMain() {
                             onChange={(event) => setSearch(event.target.value)}
                         />
                     </div>
-                    <h2 >Movies List</h2>
+
                     <section>
+                        <h2 >Movies List</h2>
                         {filteredMovieList.map((movie) => (
                             <div className="card py-20" key={movie.id}>
                                 <h3>Title : {movie.title}</h3>
@@ -49,6 +60,25 @@ function AppMain() {
 
                             </div>
                         ))}
+                    </section>
+
+                    <section>
+                        <h3 className="py-20">Add your movie here</h3>
+                        <form onSubmit={createNewMovie} action="">
+                            <label className="mx-10" htmlFor="">
+                                Title
+                            </label>
+                            <input
+                                value={newMovie}
+                                onChange={(event) => setNewMovie(event.target.value)}
+                                placeholder="Write your movie here"
+                                type="text"
+                            />
+
+                            <button type="submit" className="btn ">
+                                Add movie
+                            </button>
+                        </form>
                     </section>
                 </div>
             </main>
