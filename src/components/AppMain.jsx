db0ab59b4ae09a52455d174dc45598beee1b1488
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-02 19:25:53 +0100

    Fix: style

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -14,7 +14,7 @@ function AppMain() {
 
 
     useEffect(() => {
-        const newArray = moviesListWithId.filter((movie) => 
+        const newArray = moviesListWithId.filter((movie) =>
             movie.genre.toLowerCase().includes(search.toLowerCase())
         );
         setFilteredMovieList(newArray);
@@ -25,20 +25,26 @@ function AppMain() {
             <main>
 
                 <div className='container'>
-                    <div className="py-20">
+                    <div className="flex py-20">
+                        <span class="mx-10">
+                            <svg width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.415l-3.85-3.85zm-5.242 1.656a5 5 0 1 1 0-10 5 5 0 0 1 0 10z"/>
+                            </svg>
+                        </span>
+
                         <input
                             type="search"
-                            placeholder="Search your movie here"
+                            placeholder="Search your genre here"
                             value={search}
                             onChange={(event) => setSearch(event.target.value)}
                         />
                     </div>
-                    <h1 >Movies List</h1>
+                    <h2 >Movies List</h2>
                     <section>
                         {filteredMovieList.map((movie) => (
-                            <div className="card py-10" key={movie.id}>
+                            <div className="card py-20" key={movie.id}>
                                 <h3>Title : {movie.title}</h3>
-                                <p>Id : {movie.id}</p>
+                                {/* <p>Id : {movie.id}</p> */}
                                 <p>Genre : {movie.genre}</p>
 
                             </div>
