58efcd00094b13f5d25f9a30fc9a5c8ecbb136f3
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-02 17:42:47 +0100

    Feat: Use effect wip

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,30 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { moviesList } from "../assets/moviesList";
 
 function AppMain() {
 
     const [search, setSearch] = useState("");
+    // const [filteredMovieList, setFilteredMovieList] = useState(initialMovie);
 
     const moviesListWithId = moviesList.map((movie, index) => ({
         ...movie,
         id: index + 1,
     }));
 
+    // useEffect(() => {
+    //     const newArray = filteredMovieList.filter((movie) => movie.includes(search));
+    //     setFilteredMovieList(newArray);
+    // }, [search]);
 
     return (
         <>
             <main>
-                <div>
-                    <input 
-                    type="search"
-                    className="mx-10"
-                    placeholder="Search your movie here"
-                    value={search}
-                    onChange={(event) => setSearch(event.target.value)}
-                     />
-                </div>
-                <div className='container'>
 
+                <div className='container'>
+                    <div className="py-20">
+                        <input
+                            type="search"
+                            placeholder="Search your movie here"
+                            value={search}
+                            onChange={(event) => setSearch(event.target.value)}
+                        />
+                    </div>
                     <h1 >Movies List</h1>
                     <section>
                         {moviesListWithId.map((movie) => (
