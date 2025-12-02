947312531bb1391777ded21138b6cfc56a2c512b
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-02 17:00:01 +0100

    Feat: Search input

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { moviesList } from "../assets/moviesList";
 
 function AppMain() {
+
+    const [search, setSearch] = useState("");
 
     const moviesListWithId = moviesList.map((movie, index) => ({
         ...movie,
@@ -11,9 +14,18 @@ function AppMain() {
     return (
         <>
             <main>
+                <div>
+                    <input 
+                    type="search"
+                    className="mx-10"
+                    placeholder="Search your movie here"
+                    value={search}
+                    onChange={(event) => setSearch(event.target.value)}
+                     />
+                </div>
                 <div className='container'>
 
-                    <h1 className="py-20">Movies List</h1>
+                    <h1 >Movies List</h1>
                     <section>
                         {moviesListWithId.map((movie) => (
                             <div className="card py-10" key={movie.id}>
