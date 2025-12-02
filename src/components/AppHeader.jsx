c24e33c97d4dc8cccaf10f0cb5e27f0470269b90
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-02 14:54:57 +0100

    Feat:general settings

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,8 @@
+export default function AppHeader() {
+
+    return (
+        <header className="py-20 flex">
+            <h1>Header</h1>
+        </header>
+    );
+}
---
+++ src/components/AppMain.jsx
@@ -0,0 +1,23 @@
+
+
+function AppMain() {
+
+    return (
+        <>
+            <main>
+                <div className='container'>
+                    <h2>Product 1</h2>
+                    <h2>Product 2</h2>
+                 
+
+                    <form >
+
+
+                    </form>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default AppMain;
